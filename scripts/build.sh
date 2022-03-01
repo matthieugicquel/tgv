c32a0af9018f005c9647c453ae36cbf1949688a8
Matthieu Gicquel <matthieug@hey.com>
2022-03-01 21:09:49 +0100

    commented out faster version with bundling

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,27 @@
+mkdir -p dist
+
 sucrase ./src -d ./dist --transforms typescript --quiet
+
+# ./node_modules/.bin/esbuild src/dev-server-command.ts \
+#   --bundle \
+#   --outfile=./dist/dev-server-command.js\
+#   --platform=node \
+#   --format=esm \
+#   --external:esbuild \
+#   --external:@swc/core \
+#   --banner:js="import { createRequire as globalCreateRequire } from 'module';const require = globalCreateRequire(import.meta.url);" \
+
+
+
+# ./node_modules/.bin/esbuild src/prod-bundler-command.ts \
+#   --bundle \
+#   --outfile=./dist/prod-bundler-command.js\
+#   --platform=node \
+#   --format=esm \
+#   --external:esbuild \
+#   --external:@swc/core \
+#   --banner:js="import { createRequire as globalCreateRequire } from 'module';const require = globalCreateRequire(import.meta.url);" \
+
+# TODO: transpile and copy workers
 
 echo '{"type": "module"}' > ./dist/package.json
